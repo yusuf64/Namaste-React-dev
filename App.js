c59bd2cfe5569ec8fcaf6952f398b5858bf0a0a9
Yusuf Khan <ky69452@gmail.com>
2023-08-06 16:22:18 +0530

    added a new chapter

---
+++ App.js
@@ -0,0 +1,78 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// For creating an element we will use REACT method, create element, it will take three arguments 1. the tag that we want to create  2. the object in which we can add attribute to our element 3. the content inside our tag
+
+const heading = React.createElement(
+  "h1",
+  { className: "heading" },
+  "Hello World From React !"
+);
+
+// For rendering a element we need to first create a root element in which we eill render all the stuff and for that we will use REACTDOM
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// For putting the heding inside the root div we weill simply use ReactDom method render() and in that we will pass the argument whcih we want to render
+
+root.render(heading);
+
+// as you can see we created a javascirp object usiing React method cReateElement()
+// and after that we created a root
+// and the render method is just converting that heading object into html and putting it into the root div
+
+// Now this was just a single element what if we want to create a nested element a seciotn or landing page hwo can we do this, will find out soon :)
+
+// let us find it out
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am inside a child element")
+  )
+);
+
+// Remember the React.createElement is not a not creating a html element its just a JS object , the ReactDOM.render is used convert that object into html element
+
+// Now what if we want to pass more than one children of a specific parent
+// we can do this by passing an arry of children into third argument
+
+const parent2 = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am inside a child element"),
+    React.createElement("h2", {}, "I am inside a child element"),
+  ])
+);
+
+root.render(parent2);
+
+console.log(parent2);
+
+// Now lets make it more complext
+
+const parent3 = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am inside a child element"),
+    React.createElement("h2", {}, "I am inside a child element"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am inside a child element"),
+    React.createElement("h2", {}, "I am inside a child element"),
+  ]),
+]);
+
+// so this how we can create nested childs in react without using JSX
+// but but but how can we use this its very ugly and what if we have more nested ocmplex html we cant even read this to solve this problem we will use JSX
+
+// WILL soon learn about JSX till then enjoy :)DD
+
+const parent4 = React.createElement("div", { id: "new" }, [
+  React.createElement("h1", { id: "new" }, "heading"),
+]);
+
+// what if we have already have some content inside root elmenet , root.render replaces everyting in that root element with the content we will add using root.render(heaindg);
---
+++ Chapter 3 - Laying the Foundation/notes.md
@@ -0,0 +1,8 @@
+**Chapter 3 - Laying the Foundation (6th August 23)**
+
+# How can we create our own script to execute the parcel instead of writing the whole command?
+
+- We can create our own script in package.json in script object, type the name you want to give to your script and the value will be the real command that is used to execute that packagge
+
+- After creating your own script you can execute it by using this command
+  `npm run your-command-name` OR `npm your-command-name`
